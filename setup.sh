363ec8f30aabb2e66140764f21595c8fd27e4099
Jan Ryklikas <jan_ryklikas@ymail.com>
2024-03-11 20:21:56 +0100

    update install script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+TIMESTAMP=$(date +%s)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DST_CONFIG_DIR=~/printer_data/config
+SRC_CONFIG_DIR=$SCRIPT_DIR/config
 
 source $SCRIPT_DIR/etc/vars.sh
 source $SCRIPT_DIR/etc/wizard.sh
 
 sudo service klipper stop
 
-cp -rf $SCRIPT_DIR/config/* ~/printer_data/config/
-mv $SCRIPT_DIR/config/printer_$PRINTER.cfg ~/printer_data/config/printer.cfg
+# Override all universal files
+mkdir -p $DST_CONFIG_DIR/PrinterOverrides
+cp -rf  $SRC_CONFIG_DIR/PrinterOverrides/$PRINTER $DST_CONFIG_DIR/PrinterOverrides/
+cp -rf  $SRC_CONFIG_DIR/UniversalMacros \ 
+        $SRC_CONFIG_DIR/OrcaSlicerPresets \ 
+        $SRC_CONFIG_DIR/printer_$PRINTER.cfg \ 
+        $DST_CONFIG_DIR/
+        
+# Don't override printer.cfg it might have changed locally
+cp -n $DST_CONFIG_DIR/printer_$PRINTER.cfg $DST_CONFIG_DIR/printer.cfg
 
 sudo service klipper start
